dcb0571fc8a96ed6aa6c285dca51bd8e7393ce28
Isabelle Weir <iweir@sdac.harvard.edu>
2020-06-11 17:51:17 -0400

    Update FUN_AUCiw.R

--- FUN_AUCiw.R
+++ FUN_AUCiw.R
@@ -3,13 +3,13 @@ AUCiw <- function(x, y, na.rm = FALSE, to=max(x), from=min(x)) {
   if (length(x) != length(y))
     stop("length x must equal length y")
   
-  values <- approx(x, y, xout = sort(unique(c(from, to, x[x > from & x < to]))))
-  idx <- order(values$x)
+  values   <- approx(x, y, xout = sort(unique(c(from, to, x[x > from & x < to]))))
+  idx      <- order(values$x)
   values$x <- values$x[idx]
   values$y <- values$y[idx]
   
   # use step function summation
-  a <- sum( values$y[-length(values$y)] * (values$x[-1] - values$x[-length(values$x)]))
+  a        <- sum( values$y[-length(values$y)] * (values$x[-1] - values$x[-length(values$x)]))
   return(a)
 }
 
